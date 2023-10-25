54ee1328331ab773e55f5ae922870c099ce12e57
shrishj <shrishj@stanford.edu>
2023-10-25 15:54:19 -0700

    Regression on years greater than 1960 (#3)

--- analysis/code/analyze_data.py
+++ analysis/code/analyze_data.py
@@ -7,11 +7,19 @@ def main():
     df = import_data()
     fit = run_regression(df)
     formatted = format_model(fit)
-    
+
+    df = import_data()
+    fit_subset = run_regression_subset(df)
+    formatted_subset = format_model(fit_subset)
+
     with open('output/regression.csv', 'w') as f:
         f.write('<tab:regression>' + '\n')
         formatted.to_csv(f, sep = '\t', index = False, header = False)
-    
+
+    with open('output/regression_subset.csv', 'w') as f:
+        f.write('<tab:regression>' + '\n')
+        formatted_subset.to_csv(f, sep = '\t', index = False, header = False) 
+
 def import_data():
     df = pd.read_csv('input/data_cleaned.csv')
     df['post_tv'] = df['year'] > df['year_tv_introduced']
@@ -24,7 +32,17 @@ def run_regression(df):
     fit = model.fit()
     
     return(fit)
+
+# Creating a new function to handle regression for years greater than 1960    
+def run_regression_subset(df):
+    df_subset = df[df['year'] >= 1960]
     
+    df_subset = df_subset.set_index(['county_id', 'year'])
+    model = PanelOLS.from_formula('chips_sold ~ 1 + post_tv + EntityEffects + TimeEffects', data=df_subset)
+    fit = model.fit()
+
+    return fit   
+      
 def format_model(fit):
     formatted = pd.DataFrame({'coef'     : fit.params, 
                               'std_error': fit.std_errors, 
